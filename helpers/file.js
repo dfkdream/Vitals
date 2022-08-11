978aa28556e43974aaf0b91864aa394c6b754c25
dfkdream <dfkdream@gmail.com>
2022-08-11 09:40:57 +0900

    Add blocklist to file.js helper

--- helpers/file.js
+++ helpers/file.js
@@ -4,6 +4,28 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 Me.imports.helpers.polyfills;
 const ByteArray = imports.byteArray;
 
+let blocklist = [
+    "/sys/class/hwmon/hwmon2/fan1_input",
+    "/sys/class/hwmon/hwmon2/fan3_input",
+    "/sys/class/hwmon/hwmon2/fan4_input",
+    "/sys/class/hwmon/hwmon2/fan5_input",
+    "/sys/class/hwmon/hwmon2/temp1_input",  // SYSTIN
+    "/sys/class/hwmon/hwmon2/temp4_input",  // AUXTIN1
+    "/sys/class/hwmon/hwmon2/temp5_input",  // AUXTIN2
+    "/sys/class/hwmon/hwmon2/temp6_input",  // AUXTIN3
+    "/sys/class/hwmon/hwmon2/temp8_input",  // PCH_CHIP_CPU_MAX_TEMP
+    "/sys/class/hwmon/hwmon2/temp9_input",  // PCH_CHIP_TEMP
+    "/sys/class/hwmon/hwmon2/temp10_input", // PCH_CPU_TEMP
+];
+
+function isInBlockList(path){
+    return blocklist.includes(path);
+}
+
+function addBlockList(path){
+    blocklist.push(path);
+}
+
 function getcontents(filename) {
     let handle = Gio.File.new_for_path(filename);
     let contents = handle.load_contents(null)[1];
@@ -59,6 +81,11 @@ File.prototype.list = function() {
                         try {
                             let files = enumerator.next_files_finish(res);
                             for (let i = 0; i < files.length; i++) {
+                                let fullPath = `${file.peek_path()}/${files[i].get_name()}`;
+                                if (isInBlockList(fullPath)){
+                                    //global.log(`file-list blocked: ${fullPath}`);
+                                    continue;
+                                }
                                 results.push(files[i].get_attribute_as_string(Gio.FILE_ATTRIBUTE_STANDARD_NAME));
                             }
 
